2b9b8200398be5a49cebd4731e4a4f2d366252ae
to.kremser <to.kremser@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2009-10-04 14:52:11 +0000

    Plot bevölkert und fertig

---
+++ trunk/Ringlaser/plot/5V_1.C
@@ -0,0 +1,38 @@
+//Plot
+{
+//Standart Settings
+gROOT->Reset();
+gROOT->SetStyle("Plain");
+gStyle->SetOptFit();
+//Load Data
+#include "Riostream.h"
+ifstream in;
+in.open("/home/paule/Desktop/FPI/Ringlaser/.data/5V_1.dat");
+const Int_t ndata = 11;
+Float_t x[ndata];
+Float_t y[ndata];
+Float_t sx[ndata];
+Float_t sy[ndata];
+Int_t line = 0;
+while (1) 
+{
+  in >> x[line] >> sx[line] >> y[line] >> sy[line];
+  cout<<"sy:"<<sy[line]<<" y:"<<y[line]<<" sx:"<<sx[line]<<" x:"<<x[line]<<endl;
+  if (!in.good()) break;
+  line++;
+}
+cout<<"Found "<<line<<" Data Points."<<endl;
+in.close();
+//plot&fit
+TGraphErrors *gr = new TGraphErrors(line,x,y,sx,sy);
+TF1* lin = new TF1("linear","pol1",x[0],x[ndata]);
+lin->SetLineWidth(2);
+lin->SetLineColor(2);
+gr->SetTitle("Constant Rotation at 5V, first");
+gr->GetXaxis()->SetTitle("Durchtrittspunkt[mm]");
+gr->GetYaxis()->SetTitle("Schwebungsfrequenz[kHz]");
+gr->SetMarkerColor(4);
+gr->SetMarkerStyle(20);
+gr->Fit("linear");
+gr->Draw("ALP");
+}
