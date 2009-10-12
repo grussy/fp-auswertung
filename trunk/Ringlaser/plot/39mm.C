a473c667e74fd8ec893f30249f566049647e7119
to.kremser <to.kremser@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2009-10-12 08:01:09 +0000

    ...

--- trunk/Ringlaser/plot/39mm.C
+++ trunk/Ringlaser/plot/39mm.C
@@ -7,7 +7,7 @@ gStyle->SetOptFit();
 //Load Data
 #include "Riostream.h"
 ifstream in;
-in.open("/home/paule/Desktop/FP/trunk/Ringlaser/plot/data/39mm.dat");
+in.open("/home/paule/FP/trunk/Ringlaser/plot/data/39mm.dat");
 const Int_t ndata = 10;
 Float_t x[ndata];
 Float_t y[ndata];
