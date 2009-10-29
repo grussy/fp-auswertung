be63810970554492c28850df9d11fe0bf8177e45
to.kremser <to.kremser@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2009-10-29 16:12:54 +0000

    Die Skripte schleife,.py und gegenstaende.py laufen, sind noch sehr schlechte Werte besonders für Schleife (bei dem andern kammas nicht überprüfen). Hab alles überarbeitet, die ham noch ganz anders gemessen und hatten auch andere Wiederstände etc also kanns gut sein das doch noch ein Fehler drinsteckt der die abweichung vom theoretischen Wert sowie den Hohen Fehler erklährt (hoher Fehler kommt auch aus der Höhe mit 22%)
    Ich geh jeze boardn, bis montag.

--- squid/plot/schleife.py
+++ squid/plot/schleife.py
@@ -15,78 +15,66 @@ gROOT.SetStyle("Plain")
 
 # Lade Messdaten
 msf = lade_schleife()
-print '\nAus einem ersten Fit an R1...R4 wurde das gewichtete Mittel von omega zu %f +- %f bestimmt\n\n'%(omega,somega)
-#msf = filter(lambda mi: mi.fitable, ms)
+print '\nAus einem ersten Fit an R1...R4 wurde das gewichtete Mittel von omega zu %f +- %f bestimmt'%(omega,somega)
 
 # Fuehre Fits durch -------------------------------------------------
-print 'Sinus-Fits ohne Steigung:'
-for m in msf:
-        m.fito()
-	print 'Funktion fuer %s: %f + %f sin(x + %f )'%(
-		m.name,m.a,m.b,m.c)
-        print 'rchisq = %.5f, dU = %.4f +- %.4f' % (
-        	m.rchisq, m.dU, m.sdU)
+#print 'Sinus-Fits ohne Steigung:'
+#for m in msf:
+#        m.fito()
+#	print 'Funktion fuer %s: %f + %f sin(x + %f )'%(
+#		m.name,m.a,m.b,m.c)
+#        print 'rchisq = %.5f, dU = %.4f +- %.4f' % (
+#        	m.rchisq, m.dU, m.sdU)
 
 print '\nSinus-Fits mit Steigung:'
 for m in msf:
         m.fitm()
-	print 'Funktion fuer %s: %f + %f x + %f sin(x + %f )'%(
-		m.name,m.a,m.d,m.b,m.c)
+#	print 'Funktion fuer %s: %f + %f x + %f sin(x + %f )'%(
+#		m.name,m.a,m.d,m.b,m.c)
         print 'rchisq = %.5f, dU = %.4f +- %.4f' % (
         	m.rchisq, m.dU, m.sdU)
 	m.draw()
 	m.vdraw()
 
 
-# Berechne Magnetfeld Bz und die Hoehe z des SQUIDs -----------------
-print '\nBerechnung von Bz und z'
-z, sz = [], []
+# Berechne Magnetfeld Bz -----------------
+print '\nBerechnung von Bz'
 for m in msf:
     # Berechne Bz und seinen Fehler
-    Bz = m.dU/(2 * m.si * Feff)
+    Bz = (m.dU*Ffl)/m.si
     sBz = Bz * m.sdU/m.dU
     m.Bz, m.sBz = Bz, sBz
-
-    # Berechne z (der rel. Fehler von r**2 dominiert mit 20%)
-    a = mu0 * m.Ibat/(2*Bz)
-    zi = sqrt( (a*r**2)**(2./3.) - r**2 )
-    szi = abs((2./3. * a * r / (a*r**2)**(1./3.) - r) / zi * sr)
-    m.z, m.sz = zi, szi
-    z += [zi]; sz += [szi]
     
-    print '%s: Bz = %g +- %g (%.1f%%), z = %f +- %f (%.1f%%)' % (
-        m.name, Bz, sBz, sBz/Bz*100, zi, szi, szi/zi*100)
+    print '%s: Bz = (%f * Ffl)/%f = %g +- %g (%.1f%%)' % (
+        m.bez,m.dU,m.si, Bz, sBz, sBz/Bz*100)
 
-# relativer systematischer Fehler von z
-rzs = abs((1. - 1./3.*(a*r**2)**(2./3.) / ((a*r**2)**(2./3.) - r**2)) * sr/r)
-
-# Berechne gewichtetes Mittel
-zm, szm = gew_mittel(zip(z,sz))
-szms = zm * rzs
-print '\nGewichtetes Mittel von z: %f +- %f (stat) +- %f (syst)' % (
-    zm, szm, szms)
+zm, szm = (0.044, 0.01)
+print '\nAbstand Squid - Probe z: %f +- %f (%f%%)' % (
+    zm, szm,float((szm/zm)*100))
 
 
-# Berechne Magnetfeld Bz und die Hoehe z des SQUIDs -----------------
+# Berechne Dipolmomente -----------------
 print '\nBerechnung der Dipolmomente'
 for m in msf:
     # Berechne theoretischen Wert aus der Schleifenflaeche und dem Strom
     pt = m.Ibat * A
-    spt = 2 * pi * r * m.Ibat * sr
+    spt = pt * sr
     m.pt, m.spt = pt, spt
 
     # Bestimme Dipolmoment aus Bz und dem Abstand
     pm = 2*pi * zm**3 * m.Bz / mu0
-    spm = pm * sqrt( (m.sBz/m.Bz)**2 + (3.*(szm+szms)/zm)**2 )
+    spm = pm * sqrt( (m.sBz/m.Bz)**2 + (3.*(szm)/zm)**2 )
     m.pm, m.spm = pm, spm
     
-    print '%s: pt = %g +- %g, pm = %g +- %g' % (
-        m.name, pt, spt, pm, spm)
+    print '%s: pt = %g +- %g (%f%%), pm = %g +- %g (%f%%) Verhältniss Theo/Messung: %f' % (
+        m.bez, pt, spt, (spt/pt)*100, pm, spm, (spm/pm)*100, pt/pm)
+   
+print '\n --> Wie du siehst stimmen die Werte um den Faktor 1,6 nicht. Fehler viel zu gross. Finde aber keine Fehler im Code mehr'
 raw_input();
 
 # Erzeuge TeX Tabellen
 import texgen
 #texgen.write_table_schleife(msf)
-texgen.write_table_schleife_fit(msf)
-texgen.write_table_z(msf)
-texgen.write_table_schleife_dipol(msf)
+#texgen.write_table_schleife_fit(msf)
+#texgen.write_table_z(msf)
+#texgen.write_table_schleife_dipol(msf)
