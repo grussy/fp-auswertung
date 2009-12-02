3cb7360ec5948935b671b237972bccb9e43d9c62
to.grussenmeyer <to.grussenmeyer@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2009-12-02 21:11:08 +0000

    t

--- trunk/halbleiter/plot/data/bandluecke/germanium.py
+++ trunk/halbleiter/plot/data/bandluecke/germanium.py
@@ -14,12 +14,14 @@ gROOT.SetStyle("Plain")
 
 # Klasse zum Einlesen, Fitten und Plotten der Messungen
 class Messung:
-    def __init__(self, name):
+    def __init__(self, name, links, rechts):
         self.name = name
 	self.ugrund = 0
     	self.lampe = 0
 	self.xmin = 0.5
 	self.xmax = 1.
+	self.links = links
+	self.rechts = rechts
         # Lese Messdaten ein	
 	self.time = []
 	self.winkel = []
@@ -50,6 +52,23 @@ class Messung:
         g.SetMarkerStyle(3)
 	g.SetMarkerColor(2)
         g.SetMarkerSize(1.0)
+	f = TF1('f', '[0]', -10, 10)
+	f.SetParameter(0, findMaximum(self.absor, self.energie, self.links - 0.5, self.rechts + 0.5))
+	f2 = TF1('f', '[0]', -10, 10)
+	f2.SetParameter(0, findMinimum(self.trans, self.energie, self.links - 0.5, self.rechts + 0.5))
+
+	f3 = TF1('f', '[0] + [1]*x', -10, 10)
+	f3.SetParameter(0, 0.5)
+	f3.SetParameter(1, 1)
+	g.Fit(f3, 'Q', "lalala", self.links, self.rechts)
+	f3.SetRange(-10., 10.)
+
+
+
+	self.func1 = f
+	self.func2 = f2
+	self.func3 = f3
+
         self.graph = g
 	
 
@@ -60,6 +79,14 @@ class Messung:
         g2.SetMarkerStyle(2)
 	g2.SetMarkerColor(4)
         g2.SetMarkerSize(1.0)
+
+	f4 = TF1('f', '[0] + [1]*x', -10, 10)
+	f4.SetParameter(0, 0.5)
+	f4.SetParameter(1, 1)
+	g2.Fit(f4, 'Q', "lalala", self.links, self.rechts)
+	f4.SetRange(-10., 10.)
+
+	self.func4 = f4
         self.graph2 = g2
 	h = g.GetHistogram()
 	h.SetMinimum(0)
@@ -70,8 +97,15 @@ class Messung:
        	c = TCanvas('c_'+title, title)
        	self.canvas = c
        	c.SetGrid()
+	#self.func1.Draw('C')
        	self.graph.Draw('AP')
 	self.graph2.Draw('P')
+	#c.Add(self.func1)
+	self.func1.Draw('SAME')
+	self.func2.Draw('SAME')
+	self.func3.Draw('SAME')
+	self.func3.Draw('SAME')
+	self.func4.Draw('SAME')
        	c.Update()
     
     def setAxis(self, x,y):
@@ -146,14 +180,14 @@ def leistunglampe(winkel, lampe):
 	steigung = (yrechts - ylinks) / (xrechts - xlinks)
 	return (winkel - xlinks) * steigung + ylinks
 
-def ladeMessung(messung, untergrund, spektrum, xmin, xmax):
-	mess1 = Messung(messung)
+def ladeMessung(messung, untergrund, spektrum, xmin, xmax, links, rechts):
+	mess1 = Messung(messung, links, rechts)
 	mess1.setAxis(xmin, xmax)
 	
 	# Lade Untergrund
-	mess1.ugrund = Messung(untergrund)
+	mess1.ugrund = Messung(untergrund, links, rechts)
 	# Lade Lampenspektrum
-	mess1.lampe = Messung(spektrum)
+	mess1.lampe = Messung(spektrum, links, rechts)
 
 	for i in range(len(mess1.time)):
 		winkel = mess1.winkel[i]
@@ -163,21 +197,39 @@ def ladeMessung(messung, untergrund, spektrum, xmin, xmax):
 		mess1.absor[i] = absor
 	return mess1
 
+def findMaximum(messreihe, winkel, links, rechts):
+	actual = 0.
+	a = 0
+	for i in messreihe:
+		if winkel[a] > links and winkel[a] < rechts and actual < i:
+			actual = i
+		a += 1
+	#print actual
+	return actual
+
+def findMinimum(messreihe, winkel, links, rechts):
+	actual = 10.
+	a = 0
+	for i in messreihe:
+		if winkel[a] > links and winkel[a] < rechts and actual > i:
+			actual = i
+		a += 1
+	print actual
+	return actual
 
 
 
+grge1 = ladeMessung( "grge1.txt", "Germanium_Untergrund.txt", "LampeLeistung.txt", 0.63, 0.75, 0.68, 0.7)
+grge1b = ladeMessung( "grge1.txt", "Germanium_Untergrund.txt", "LampeLeistung.txt", -0.76, -0.63, -0.7, -0.68)
 
-grge1 = ladeMessung( "grge1.txt", "Germanium_Untergrund.txt", "LampeLeistung.txt", 0.63, 0.75)
-grge1b = ladeMessung( "grge1.txt", "Germanium_Untergrund.txt", "LampeLeistung.txt", -0.76, -0.63)
+grge2 = ladeMessung("grge2.txt", "Germanium_Untergrund.txt", "LampeLeistung.txt", 0.63, 0.76, 0.68, 0.7)
+grge2b = ladeMessung("grge2.txt", "Germanium_Untergrund.txt", "LampeLeistung.txt", -0.75, -0.63, -0.7, -0.68)
 
-grge2 = ladeMessung("grge2.txt", "Germanium_Untergrund.txt", "LampeLeistung.txt", 0.63, 0.76)
-grge2b = ladeMessung("grge2.txt", "Germanium_Untergrund.txt", "LampeLeistung.txt", -0.75, -0.63)
+grgeb1 = ladeMessung("grgeblende1cm.txt", "Germanium_Untergrund.txt", "grspektrumblende1cm.txt", 0.63, 0.74, 0.68, 0.7)
+grgeb1b = ladeMessung("grgeblende1cm.txt", "Germanium_Untergrund.txt", "grspektrumblende1cm.txt", -0.76, -0.63, -0.7, -0.68)
 
-grgeb1 = ladeMessung("grgeblende1cm.txt", "Germanium_Untergrund.txt", "grspektrumblende1cm.txt", 0.63, 0.74)
-grgeb1b = ladeMessung("grgeblende1cm.txt", "Germanium_Untergrund.txt", "grspektrumblende1cm.txt", -0.76, -0.63)
-
-grgeb2 = ladeMessung("grgeblende1cm2.txt", "Germanium_Untergrund.txt", "grspektrumblende1cm.txt", 0.62, 0.75)
-grgeb2b = ladeMessung("grgeblende1cm2.txt", "Germanium_Untergrund.txt", "grspektrumblende1cm.txt", -0.76, -0.63)
+grgeb2 = ladeMessung("grgeblende1cm2.txt", "Germanium_Untergrund.txt", "grspektrumblende1cm.txt", 0.62, 0.75, 0.68, 0.7)
+grgeb2b = ladeMessung("grgeblende1cm2.txt", "Germanium_Untergrund.txt", "grspektrumblende1cm.txt", -0.76, -0.63, -0.7, -0.68)
 
 
 grge1.draw("grge1")
