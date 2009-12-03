3305441a227e49ea6f2fb64bf9587f8452b5995d
to.kremser <to.kremser@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2009-12-03 11:31:25 +0000

    StandardMsg

--- trunk/halbleiter/plot/messung.py
+++ trunk/halbleiter/plot/messung.py
@@ -13,13 +13,13 @@ gROOT.SetStyle("Plain")
 
 # Klasse zum Einlesen, Fitten und Plotten der Messungen
 class Messung:
-    def __init__(self, name, distance, voltage, lowerFitrange, upperFitrange, initParams):
+    def __init__(self, name, distance, voltage, lowerFitrange, upperFitrange):
         self.name = name
         self.dist = distance
         self.volts = voltage
         self.lower = float(lowerFitrange)
         self.upper = float(upperFitrange)
-        self.initParams = initParams
+        self.sdist = 0.5
 
     # Lese Messdaten ein	
         data = []
@@ -40,9 +40,9 @@ class Messung:
         self.graph = g
 
     # Gaussfit ohne Korrektur
-    def fit(self):
+    def fit(self, initParams):
         f = TF1('f_'+self.name, '[0]/sqrt(2*pi*[1]) * exp(-0.5*((x-[2])/[1])^2) + [3]', self.lower, self.upper)
-        f.SetParameters(array('d', self.initParams))
+        f.SetParameters(array('d', initParams))
         self.graph.Fit(f, 'QR')
         self.amp, self.samp = f.GetParameter(0), f.GetParError(0)
         self.sigma, self.ssigma = f.GetParameter(1), f.GetParError(1)
@@ -51,7 +51,8 @@ class Messung:
         self.chisq = f.GetChisquare()
         self.ndf = f.GetNDF()
         self.rchisq = self.chisq/self.ndf
-        print 'Amplitude: %g, sigma: %g, ort: %g, off: %g'%(self.amp, self.sigma, self.ort, self.off)
+        self.f = f
+        #print 'Amplitude: %g, sigma: %g, ort: %g, off: %g'%(self.amp, self.sigma, self.ort, self.off)
 
 
     def draw(self):
@@ -59,6 +60,7 @@ class Messung:
         self.canvas = c
         c.SetGrid()
         self.graph.Draw('AP')
+        #self.f.Draw('SAME')
         #self.test.Draw('AP')
         c.Update()
 
@@ -69,7 +71,7 @@ class Messung:
 
             
 # Hilfsroutine zum Einlesung der Daten bei Messung variablen Abstandes
-def lade_varDist(dateiname, initParams):
+def lade_Daten(dateiname):
     m = []
     for line in open(dateiname, 'r').readlines()[1:]:
         if not line.strip() or line.strip()[0] == '#': continue
@@ -79,7 +81,26 @@ def lade_varDist(dateiname, initParams):
             distance = v[3],
             voltage = v[4],
             lowerFitrange = v[5],
-            upperFitrange = v[6],
-            initParams = initParams)
+            upperFitrange = v[6])
         m += [mi]
     return m
+
+def gew_mittel(werte, fehler):
+##    gew_mittel(list(float), list(float)) -> (float, float)
+##    werte  : Liste der Messwerte 
+##    fehler : Liste der Fehler
+##    ->   Tupel (gx, sgx) aus gewichtetem Mittel und dessen Fehler
+    suma = sumb = 0.
+    for i in range(len(werte)):
+        suma += werte[i] / fehler[i]**2
+        sumb += 1. / fehler[i]**2
+    return (suma/sumb, 1/sqrt(sumb))
+
+def arith_mittel(x):
+    '''arith_mittel(list(float)) -> float
+    x  : Liste aus Messwerte
+    ->   arithmetisches Mittel der Messwerte'''
+    sumx = 0.
+    for xi in x:
+        sumx += xi
+    return sumx / len(x)
