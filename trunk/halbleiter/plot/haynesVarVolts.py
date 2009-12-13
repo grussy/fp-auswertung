48d723712c009f6ed6a2104f2cdd86a2f2fa37f2
to.kremser <to.kremser@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2009-12-13 19:50:48 +0000

    wer

--- trunk/halbleiter/plot/haynesVarVolts.py
+++ trunk/halbleiter/plot/haynesVarVolts.py
@@ -54,29 +54,34 @@ for m in msf :
         sdists.append(float(m.sdist))
         Es.append(float(m.volts)/float(m.dist))
         sEs.append(Es[len(Es)-1]*((sU/float(m.volts))+(float(m.sdist)/float(m.dist))))
-    m.draw()
+    #m.draw()
+    #m.savePlot()
     
-print '\n DEBUG: Fehler:'
-print 'Amps:'
-print samps
-print 'Sigs:'
-print ssigs
-print 'Means:'
-print smeans
+##print '\n DEBUG: Fehler:'
+##print 'Amps:'
+##print samps
+##print 'Sigs:'
+##print ssigs
+##print 'Means:'
+##print smeans
+print means[1]
 #Berechne Physik
 
 #Fitte die Schwerpunkte
-gMean = TGraphErrors(len(means), array('d',means) ,array('d',dists), array('d',smeans) ,array('d',sdists))
-gMean.SetTitle(';Zeit t [s];Ort des Schwerpunktes[m]')
+gMean = TGraphErrors(len(Es), array('d',Es) ,array('d',means), array('d',sEs) ,array('d',smeans))
+gMean.SetTitle(';E-Feld an Halbleiter [V/m];Zeit bis Schwerpunktsdetektion[t]')
 gMean.GetHistogram().SetTitleOffset(1.3, 'Y')
 gMean.SetMarkerStyle(20)
 gMean.SetMarkerColor(2)
-gMean.SetMarkerSize(0.2)
+gMean.SetMarkerSize(0.5)
 cMean = TCanvas('MeanFit', 'MeanFit')
 cMean.SetGrid()
 gMean.Draw('AP')
-flin = TF1('MeanFit', '[0]*x+[1]')
-flin.SetParameters(array('d', [0,0]))
+flin = TF1('MeanFit', '([1]/([0]*x))+[2]')
+flin.SetParameters(array('d', [1e-5,9e-6,0.1]))
+flin.SetMarkerStyle(20)
+flin.SetMarkerColor(2)
+flin.SetMarkerSize(0.5)
 gMean.Fit(flin, 'Q')
 vel, svel = flin.GetParameter(0), flin.GetParError(0)
 cMean.Update()
@@ -91,12 +96,15 @@ gAmp.SetTitle(';Zeit t [s];Amplitude der Gauskurve[V]')
 gAmp.GetHistogram().SetTitleOffset(1.3, 'Y')
 gAmp.SetMarkerStyle(20)
 gAmp.SetMarkerColor(2)
-gAmp.SetMarkerSize(0.2)
+gAmp.SetMarkerSize(0.5)
 cAmp = TCanvas('AmpFit', 'AmpFit')
 cAmp.SetGrid()
 gAmp.Draw('AP')
 fe = TF1('MeanFit', '[0]*exp(-x/[1])')
 fe.SetParameters(array('d', [5e-4,1e-5]))
+fe.SetMarkerStyle(20)
+fe.SetMarkerColor(2)
+fe.SetMarkerSize(0.5)
 gAmp.Fit(fe, 'Q')
 tau, stau = fe. GetParameter(1), fe.GetParError(1)
 cAmp.Update()
@@ -112,13 +120,16 @@ gsig.SetTitle(';Zeit t [s];Spannung U [V]')
 gsig.GetHistogram().SetTitleOffset(1.3, 'Y')
 gsig.SetMarkerStyle(20)
 gsig.SetMarkerColor(2)
-gsig.SetMarkerSize(0.2)
+gsig.SetMarkerSize(0.5)
 csig = TCanvas('SigFit', 'SigFit')
 csig.SetGrid()
 gsig.Draw('AP')
-fs = TF1('MeanFit', 'sqrt(2*[0]*x)')
+fs = TF1('MeanFit', 'sqrt(2*[0]*x)', 8e-6, 16e-6)
 fs.SetParameters(array('d', [5e-8]))
-gsig.Fit(fs, 'Q')
+fs.SetMarkerStyle(20)
+fs.SetMarkerColor(2)
+fs.SetMarkerSize(0.2)
+gsig.Fit(fs, 'QR')
 D, sD = fs.GetParameter(0), fs.GetParError(0)
 csig.Update()
 chisq = fs.GetChisquare()
@@ -131,16 +142,17 @@ print 'Fit on Sigmas: Chisquare = %g, Rchisquare= %g '%(chisq, ndf)
 #cmean.SaveAs('eps/MeansVarDist.eps' % self.name[:-4])
 
 # Endlich: Berechnung der physikalischen Grössen
-print '\nCalculating ...'
-E, sE = GewMittel(Es, sEs)
-mu = vel/E
-smu = mu*((svel/vel)+(sE/E))
-print ' Mobility:           (%g +- %g) m^2/(Vs)'%(mu, smu)
-print ' Lifetime:           (%g +- %g) s'%(tau, stau)
-print ' Diffusionconstant:  (%g +- %g)m^2/s'%(D,sD)
+##print '\nCalculating ...'
+##E, sE = GewMittel(Es, sEs)
+##mu = vel/E
+##smu = mu*((svel/vel)+(sE/E))
+##print ' Mobility:           (%g +- %g) m^2/(Vs)'%(mu, smu)
+##print ' Lifetime:           (%g +- %g) s'%(tau, stau)
+##print ' Diffusionconstant:  (%g +- %g)m^2/s'%(D,sD)
 
    
 print'\nDone. Press any Key.' 
+    
 raw_input();
 
 
