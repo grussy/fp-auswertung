9a3ca98f4accfc42c1ab254d45266e6936e6c5e9
to.grussenmeyer <to.grussenmeyer@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2010-03-05 08:31:03 +0000

    main_wieder_hergestellt

--- fp2/moesbauer/avr/main.c
+++ fp2/moesbauer/avr/main.c
@@ -2,16 +2,54 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <avr/interrupt.h>
-
-#include "uart.h"
-#include "commands.h"
-#include "lcd.h"
  
-
+#ifndef F_CPU
+#warning "F_CPU war noch nicht definiert, wird nun nachgeholt mit 16000000"
+#define F_CPU 16000000UL  // Systemtakt in Hz - Definition als unsigned long beachten
+#endif
+#include <util/delay.h>
+#define BAUD 115200UL
+#define UBRR_VAL ((F_CPU+BAUD*8)/(BAUD*16)-1)   // clever runden
+#define BAUD_REAL (F_CPU/(16*(UBRR_VAL+1)))     // Reale Baudrate
+#define BAUD_ERROR ((BAUD_REAL*1000)/BAUD) // Fehler in Promille, 1000 = kein Fehler.
+// #if ((BAUD_ERROR<990) || (BAUD_ERROR>1010))
+//   #error Systematischer Fehler der Baudrate grösser 1% und damit zu hoch! 
+// #endif
 
 int int1, int2 = 0;
 unsigned int saved_timer = 0;
 unsigned long overflows, saved_overflows=0;
+
+ 
+void USART_Init( void )
+{
+  /* Set baud rate */
+  UBRRH = (unsigned char)(UBRR_VAL>>8);
+  UBRRL = (unsigned char)UBRR_VAL;
+  /* Enable receiver and transmitter */
+  UCSRB = (1<<RXEN)|(1<<TXEN);
+  UCSRC |= (1<<URSEL)|(1 << UCSZ1)|(1 << UCSZ0); // Asynchron 8N1 
+}
+ 
+// bei neueren AVRs andere Bezeichnung fuer die Statusregister, hier ATmega16:
+int uart_putc(unsigned char c)
+{
+    while (!(UCSRA & (1<<UDRE)))  /* warten bis Senden moeglich */
+	;
+    UDR = c;                      /* sende Zeichen */
+    return 0;
+}
+ 
+ 
+/* puts ist unabhaengig vom Controllertyp */
+void uart_puts (char *s)
+{
+    while (*s)
+    {   /* so lange *s != '\0' also ungleich dem "String-Endezeichen" */
+        uart_putc(*s);
+        s++;
+    }
+}
 
 void init_Interrupts( void )
 {
@@ -21,9 +59,29 @@ void init_Interrupts( void )
 	GIMSK  |= (1<<INT0)|(1<<INT1);
 }
 
+ISR(INT0_vect)
+{
+	TCCR1B &= ~(1<<CS10); // stop timer
+	saved_timer = TCNT1; //timer auslesen
+	saved_overflows = overflows; // overflows auslesen
+	overflows = 0; //reset
+	TCNT1H = 0;
+	TCNT1L = 0;
+	TCCR1B |= (1<<CS10); // no prescaler, start timer
+	int1 = 1;	//flag setzen
+}
 
-
-
+ISR(INT1_vect)
+{
+	TCCR1B &= ~(1<<CS10); // stop timer
+	saved_timer = TCNT1; //timer auslesen
+	saved_overflows = overflows; // overflows auslesen
+	overflows = 0; //reset
+	TCNT1H = 0;
+	TCNT1L = 0;
+	TCCR1B |= (1<<CS10); // no prescaler, start timer
+	int2 = 1;
+}
 
 ISR(TIMER1_OVF_vect)
 {
@@ -41,31 +99,26 @@ int main(void)
 {
 	char buffer [20];
 	USART_Init();
-	lcd_init();
-
-	//init_Interrupts();
-	//start_Timer();
+	uart_puts("\n#########################################################");
+	uart_puts("\n# Welcome to Low Speed Meassuring using a Mouse Sensor. #");
+	uart_puts("\n# one overflow means 65535 timer ticks (16 bit) and we  #");
+	uart_puts("\n# have no prescaler so one tick means 1/16Mhz           #");
+	uart_puts("\n#                     by Tobi and Paul                  #");
+	uart_puts("\n#########################################################");
+	uart_puts("\n");
+	init_Interrupts();
+	start_Timer();
 	sei();
-
-	lcd_data('T');
-    	lcd_data('e');
-    	lcd_data('s');
-    	lcd_data('t');
- 
-    	set_cursor(0,2);
- 
-    	lcd_string("Hello World!");
-
-	DDRD |= ( 1 << PD5 );
- 
 	while(1) {
-		//USART abfragen ob zeichen verfügbar
-		if (UCSRA & (1<<RXC)) 
-		{
-			//uart_puts("zeichen vorhanden\n");
-			identify_command();
+		if (int1) {
+			int1 = 0;
+			sprintf(buffer, " %ut%uo", saved_timer, saved_overflows);
+			uart_puts(buffer);
 		}
-		
-		PORTD = PIND ^ ( 1 << PD5 );
+		if (int2) {
+			int2 = 0;
+			sprintf(buffer, " %ut%uo", saved_timer, saved_overflows);
+			uart_puts(buffer);
+		}
 	}
 }
