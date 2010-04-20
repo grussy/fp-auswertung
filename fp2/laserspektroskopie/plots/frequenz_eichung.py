8f2ef1dc3fda4b53abbf6659ef966fd7ca3ba21e
to.kremser <to.kremser@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2010-04-20 13:06:03 +0000

    Überblick

--- fp2/laserspektroskopie/plots/frequenz_eichung.py
+++ fp2/laserspektroskopie/plots/frequenz_eichung.py
@@ -8,41 +8,67 @@ import sys; sys.path.append('/usr/lib/root/')
 from ROOT import gROOT, TCanvas, TLegend, TF1, TH1F, TGraph, TMultiGraph, TGraphErrors
 gROOT.SetStyle("Plain")
 
-#################################################
-# Eichung der Zeitachse zur Frequenz.
-#################################################
-f = []
-t =[]
-for line in open('../daten/frequenz_eichung.dat','r'):
-    if not line.strip() or line.strip()[0] == '#': continue
-    t.append(float(line.split()[0]))
-    f.append(float(line.split()[1]))
-print f
-print t
 
-count = len(t)
-sf = [0.0001 for i in range(count)]
-st = [1.5 for i in range(count)]
-g = TGraphErrors(count, array('d',f) ,array('d',t), 
-            array('d',[1e-4]*count), array('d',[1.5]*count))
-g.SetTitle('Frequenzeichung; t / us; f / MHz;')
-c = TCanvas('Frequenz Eichung', 'Frequenzeichung')
+# constants
+c = 299792458 # m/s
+n = 1.000292 # brechzahl luft bodennah
+
+#functions
+#mittelwert, fehler
+def mw(data):
+    print data
+    retval = [0,0]
+    try:
+        sum, squaresum = 0.0, 0.0
+        for value in data:
+            print 'Trying Value %s ...'%(value)
+            sum += float(value)
+            print 'converted.'
+        count = len(data)
+        print count
+        print '%f  %f'%(sum, squaresum)
+        r = sum/count
+        print 'ok'        
+        sr = sqrt(squaresum/(count*(count-1)))
+        print r
+        retval = [r, sr]
+    except ValueError:
+        print "Error while converting data!"
+    return retval     
+        
+# data
+freqs = []
+times = []
+for line in open('../data/eichung_frequenz.dat','r'):
+    freqs.append(float(line.split()[0]))
+    times.append(float(line.split()[1]))
+count = len(times)
+
+# fit and draw
+g = TGraphErrors(count, array('d',times) ,array('d',freqs),array('d',[0.75]*count),
+                 array('d',[0,0001]*count))
+g.SetTitle(';time;frequency')
+g.SetMarkerStyle(1)
+g.SetMarkerColor(2)
+g.SetMarkerSize(3.0)
+f = TF1('Eichgerade', '[0]*x')
+f.SetMarkerColor(3)
+f.SetParameter(0, 1)
+g.Fit(f, 'M')
+c = TCanvas('Eichung', 'eichung')
 c.SetGrid()
-fit = TF1('Eichgerade', '[0]*x')
-#fit.SetParameters(1,1)
-fit.SetMarkerColor(2)
-g.Fit(fit, 'EMQ')
-a, sa = fit.GetParameter(0), fit.GetParError(0)
-#b, sb = fit.GetParameter(1), fit.GetParError(1)
-chisq = fit.GetChisquare()
-g.Draw('A*')
+g.Draw('AP')
 c.Update()
 
-print "Variable |    Wert     |     Fehler   |    prozent "
-print "  a            %.4e    |      %.4e    |      %.4f  "%(a, sa, sa/a)
-#print "  b            %.4e    |      %.4e    |      %.4f  "%(b, sb, sb/b)
-print ""
-print "Chisquare was  %.4f"%(chisq)
+# Calculate Free Spectral Range of Resonator
+delta_6 = 7.44 #us
+delta_15 = 19.4
+delta_3 = 3.72
 
-print "\nDone. Press Enter to continue ..."
+delta = mw([delta_6/6, delta_15/15, delta_3/3])
+print "Mittelwert für deltaFSR: %.4f +- %.4f  (%.4f per cent)"%(float(delta[0]),
+                                                         float(delta[1]),
+                                                         float(delta[1])/float(delta[0])) 
+print "\nDone. Press any Key to continue ..."
 raw_input();
+print "bye"
