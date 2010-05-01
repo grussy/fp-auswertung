8068bd410342ff17f6768f83ec7a660ce49ad9f9
to.grussenmeyer <to.grussenmeyer@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2010-05-01 22:49:07 +0000

    t

---
+++ trunk/fp2/moesbauer/messung/konst.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+'''
+Physikalische Konstanten
+'''
+
+from math import pi
+from Scientific.Physics.PhysicalQuantities import PhysicalQuantity as Q
+
+# Lichtgeschwindigkeit [m/s]
+c = Q('1c').inUnitsOf('m/s')
+
+# Wirkungsquantum
+hbar = Q('1hbar').inUnitsOf('J*s')
+
+# Energie des Gamma-Uebergangs [keV] (Quelle: www.nndc.bnl.gov)
+E0 = Q('14.4128 keV')
+
+# Kreisfrequenz des Gamma-Quants [1/s]
+omega0 = (E0/hbar).inUnitsOf('1/s')
+
+# Kernmagneton [MeV/T]
+muN = Q('3.15245166e-14 MeV/T')
