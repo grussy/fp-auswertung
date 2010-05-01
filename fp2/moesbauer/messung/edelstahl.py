ac1705c2ae9ee8b543038c216433b8f170cce3ca
to.kremser <to.kremser@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2010-05-01 01:43:37 +0000

    Eisen im Kolja Rahmen fertig: Litheraturewerte in der Nähe Absorptionsminima: 	1: (0.9118 +- 0.006577)mm/s	(4.384e-08 +- 3.162e-10)eV	(0.72%) 	2: (-0.7071 +- 0.004459)mm/s	(-3.4e-08 +- 2.143e-10)eV	(0.63%) 	3: (3.311 +- 0.05313)mm/s	(1.592e-07 +- 2.554e-09)eV	(1.60%) 	4: (-2.858 +- 0.01953)mm/s	(-1.374e-07 +- 9.389e-10)eV	(0.68%) 	5: (5.457 +- 0.02123)mm/s	(2.624e-07 +- 1.021e-09)eV	(0.39%) 	6: (-5.158 +- 0.01557)mm/s	(-2.48e-07 +- 7.486e-10)eV	(0.30%)
     Isomerieverschiebung:
    	1: (0.1023 +- 0.003973)mm/s	(4.921e-09 +- 1.91e-10)eV	(3.88%)
    	2: (0.2265 +- 0.0283)mm/s	(1.089e-08 +- 1.361e-09)eV	(12.50%)
    	3: (0.1496 +- 0.01317)mm/s	(7.19e-09 +- 6.33e-10)eV	(8.80%)
     gewichtetes Mittel:
    	   (0.1084 +- 0.00377)mm/s	(5.212e-09 +- 1.812e-10)eV	(3.48%)
    
     Kernmagnetisches Moment:
    	mua_12: (-0.1585 +- 0.0009379)muN	(0.59%)
    	mua_23: (-0.1529 +- 0.005427)muN		(3.55%)
    	mua_31: (-0.1578 +- 0.0004081)muN	(0.26%)
    
     Magnetfeld am Kernort:
    	B1: (31.81 +- 0.1751)T	(0.55%)
    	B2: (33.11 +- 0.3046)T	(0.92%)
    	B3: (32.99 +- 0.08666)T	(0.26%)
     gewichtetes Mittel:
    	    (32.78 +- 0.07526)T		(0.23%)

--- fp2/moesbauer/messung/edelstahl.py
+++ fp2/moesbauer/messung/edelstahl.py
@@ -15,28 +15,32 @@ gROOT.SetStyle("Plain")
 # 
 ##########################################################################################
 
-# Messdatei 
+# Variablen, Datenfelder etc
 messdaten = 'edelstahl_all.dat' #In dieser Datei befinden sich (bald) alle Messdaten / Unsortiert
-untergrund = 50.38      #Untergrundrate für das Messfenster in cps
-velo = []
-time = []
-counts = []
-rates = []
+untergrund = 50.38              #Untergrundrate für das Messfenster in cps
+velo = []                       #Geschwindigkeiten des Schlittens
+time = []                       #Dauer der Messung
+counts = []                     #Gezählte Ereignisse
+rates = []                      #Raten [counts/second]
+srates = []                     #Fehler auf Raten
+svelo = []                      #Fehler auf Geschwindigkeiten (bestimmt mit Maussensor)
 
+#Lade Daten:
+print "Loading Data in %s ..."%(messdaten)
 for line in open(messdaten):
     buffer = line.split()
-##    print buffer
     velo.append(float(buffer[0]))
     time.append(float(buffer[1]))
     counts.append(float(buffer[2]))
     rates.append((float(buffer[2]) / (float(buffer[1]) / 1000)) - untergrund)
-    
-
 length = len(velo)
+assert length > 0
+print "   found %i datapoints."%(length)
 
-print "Edelstahlabsorber, Messdaten: %s, Untergrund: %s cps" % (messdaten, untergrund)
+#print "Edelstahlabsorber, Messdaten: %s, Untergrund: %s cps" % (messdaten, untergrund)
 
 #Plotte die Daten:
+print "\nFitting and Drawing ..."
 Fenster = TCanvas('cr', 'Edelstahlabsorber')
 Fenster.SetGrid()
 gr = TGraph(length, array('d',velo), array('d', rates))
@@ -47,81 +51,65 @@ gr.SetMarkerColor(2)
 gr.SetMarkerStyle(3)
 gr.Draw('AP')
 
-
-# Fuehre Lorentz-Fit durch ------------------------------------------------
-
+# Fuehre Lorentz-Fit durch:
 fl = TF1('fl', '[0] + [1]*TMath::BreitWigner(x, [2], [3])')
 fl.SetLineColor(3); fl.SetLineStyle(2); fl.SetLineWidth(2)
-
 params = [
     (0, 'y_{0}',   270),
     (1, 'A',       -28),
     (2, '#mu',    0.2),
     (3, '#Gamma',  0.4) ]
-
 for i, pn, pv in params:
     fl.SetParName(i, pn)
     fl.SetParameter(i, pv)
-
-print '\nLorentz-Fit:'
 gr.Fit(fl, 'Q+')
-print_fit_result(fl)
+#print_fit_result(fl)
 lgl = create_fit_legend(
     fl, 'f(x)', 'Lorentz-Fit', lpos = (0.58, 0.41, 0.88, 0.64))
 lgl.Draw()
+print "   done with lorentz."
 
-
-# Fuehre Gauss-Fit durch --------------------------------------------------
-
+# Fuehre Gauss-Fit durch:
 fg = TF1('fg', '[0] + [1]*TMath::Gaus(x, [2], [3])')
 fg.SetLineColor(4); fg.SetLineStyle(4); fg.SetLineWidth(2)
-
 params = [
     (0, 'y_{0}',   267),
     (1, 'A',       -38),
     (2, '#mu',    0.2),
     (3, '#sigma',  0.2) ]
-
 for i, pn, pv in params:
     fg.SetParName(i, pn)
     fg.SetParameter(i, pv)
-
-print '\nGauss-Fit:'
 gr.Fit(fg, 'Q+')
-print_fit_result(fg)
+#print_fit_result(fg)
 lgg = create_fit_legend(
     fg, 'f(x)', 'Gauss-Fit', lpos = (0.58, 0.16, 0.88, 0.38))
 lgg.Draw()
+print "   done with gauss."
 
-
-# Fuehre Voigt-Fit durch --------------------------------------------------
-
+# Fuehre Voigt-Fit durch:
 fv = TF1('fv', '[0] + [1]*TMath::Voigt(x-[2], [3], [4])')
 fv.SetLineColor(1); fv.SetLineStyle(1); fv.SetLineWidth(2)
 fv.SetNpx(1000)
-
 params = [
     (0, 'y_{0}',   269), # 135
     (1, 'A',       -25),
     (2, '#mu',    0.22),
     (3, '#sigma',  0.13),
     (4, '#Gamma',  0.26) ]
-
 for i, pn, pv in params:
     fv.SetParName(i, pn)
     fv.SetParameter(i, pv)
-
-print '\nVoigt-Fit:'
 gr.Fit(fv, 'Q+')
-print_fit_result(fv)
+#print_fit_result(fv)
 lgv = create_fit_legend(
     fv, 'f(x)', 'Faltung von Lorentz und Gauss',
     lpos = (0.13, 0.16, 0.43, 0.38))
 lgv.Draw()
-
-
-
+print "   done with voigt."
 Fenster.Update()
+print "   all done."
+
 
 print "\nDone. Press Enter to continue ..."
 raw_input();
