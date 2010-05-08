ac2848e312fa0c9ba59c8e2b48b150fc1fa6b615
to.grussenmeyer <to.grussenmeyer@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2010-05-08 20:58:09 +0000

    t

---
+++ trunk/fp2/laserspektroskopie/plots/dopplerverbreitert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from array import array
+from math import pi, cos, sin, sqrt, exp
+import sys; sys.path.append('/usr/lib/root/')
+from messung import Messung
+from fits import nGauss
+from ROOT import gROOT, TCanvas, TLegend, TF1, TH1F, TGraph, TMultiGraph, TGraphErrors
+import os.path
+
+#############################################################################
+# frequenzeichung
+
+#############################################################################
+# fsr
+
+#############################################################################
+# Dopplerverbreitertes Spektrum
