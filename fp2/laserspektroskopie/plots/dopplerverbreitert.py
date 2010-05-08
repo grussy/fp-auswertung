42c5d0de6fa212bb3b628502d377f9c53ffac1af
to.grussenmeyer <to.grussenmeyer@2fb22308-b0e4-11de-b5cd-01dc870c4969>
2010-05-08 22:44:33 +0000

    t

--- fp2/laserspektroskopie/plots/dopplerverbreitert.py
+++ fp2/laserspektroskopie/plots/dopplerverbreitert.py
@@ -5,15 +5,26 @@ from array import array
 from math import pi, cos, sin, sqrt, exp
 import sys; sys.path.append('/usr/lib/root/')
 from messung import Messung
-from fits import nGauss
+from fits import nGauss, nLorenz
 from ROOT import gROOT, TCanvas, TLegend, TF1, TH1F, TGraph, TMultiGraph, TGraphErrors
 import os.path
 
 #############################################################################
-# frequenzeichung
+# frequenzeichung (Umrechnung von s in Hz bei Fsr)
+umrechnungsfaktor = 0.1
 
 #############################################################################
 # fsr
+fsr = Messung('14', 'Frequenzlineal', 0.02)
+fsr.plot()
+# Frequenzabstand in s
+freqLinealSec = fsr.freqLineal
+freqLinealHz = freqLinealSec * umrechnungsfaktor
+print 'FSR / Hz: %.4e' % freqLinealHz
 
+# Finesse
+finesse = nLorenz('14', 'Finesse der Cavity', 1)
+raw_input();
+finesse.saveParameters()
 #############################################################################
 # Dopplerverbreitertes Spektrum
